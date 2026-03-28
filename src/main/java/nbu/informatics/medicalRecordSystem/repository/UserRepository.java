2476f48f402324f37fbbbe7028665160332ab2f2
Kalin Toshev <kalin.toshev84@gmail.com>
2026-03-28 21:46:14 +0200

    Add method to UserRepository for checking existence of username

--- src/main/java/nbu/informatics/medicalRecordSystem/repository/UserRepository.java
+++ src/main/java/nbu/informatics/medicalRecordSystem/repository/UserRepository.java
@@ -9,5 +9,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
     List<User> findByRole(Role role);
+
+    boolean existsByUsername(String username);
 }
