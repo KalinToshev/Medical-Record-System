da0c7c4c3c5806e13f92d4ec87635cd93ed87356
Kalin Toshev <kalin.toshev84@gmail.com>
2026-03-28 21:39:30 +0200

    Refactor DoctorController to include RedirectAttributes for improved redirection handling

--- src/main/java/nbu/informatics/medicalRecordSystem/controller/DoctorController.java
+++ src/main/java/nbu/informatics/medicalRecordSystem/controller/DoctorController.java
@@ -11,13 +11,13 @@ import nbu.informatics.medicalRecordSystem.service.DoctorService;
 import nbu.informatics.medicalRecordSystem.service.SpecialityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/doctors")
