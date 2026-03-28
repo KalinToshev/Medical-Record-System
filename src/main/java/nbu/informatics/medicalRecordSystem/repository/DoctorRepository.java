a786d12c7e95a04ddf0dda2630432951f21a7f8e
Kalin Toshev <kalin.toshev84@gmail.com>
2026-03-28 21:45:19 +0200

    Add method to find Doctor by User and count by speciality ID

--- src/main/java/nbu/informatics/medicalRecordSystem/repository/DoctorRepository.java
+++ src/main/java/nbu/informatics/medicalRecordSystem/repository/DoctorRepository.java
@@ -1,10 +1,16 @@
 package nbu.informatics.medicalRecordSystem.repository;
 
 import nbu.informatics.medicalRecordSystem.model.entity.Doctor;
+import nbu.informatics.medicalRecordSystem.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     List<Doctor> findByIsGpTrue();
+
+    Optional<Doctor> findByUser(User user);
+
+    long countBySpecialityId(Long specialityId);
 }
